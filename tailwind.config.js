7eab37bcf677aa639e871c356b0f134cf0df8d4b
John Paul Thomas <JohnPaulThomas2016@outlook.com>
2023-11-15 22:17:07 -0600

    Project 4

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './core/home.html',
+    './core/bizet.html',
+    './core/mozart.html',
+    './core/puccini.html',
+    './core/reflection.html',
+    './core/verdi.html',
+    './core/wagner.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
